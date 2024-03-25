7d70b0bd7642d51b43ebfc79e87095185a86a981
Unai Beristain <unaiberistain@avanzosc.es>
2024-03-25 15:10:03 +0100

    [MIG] mrp_repair_proforma_report: Migration to 16.0

--- mrp_repair_proforma_report/__manifest__.py
+++ mrp_repair_proforma_report/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # (c) 2015 Esther Martín - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     "name": "Mrp Repair - Proforma Report",
-    "version": "8.0.1.0.0",
+    "version": "16.0.1.0.0",
     "depends": [
         "mrp_repair",
     ],
--- mrp_repair_proforma_report/models/mrp_repair.py
+++ mrp_repair_proforma_report/models/mrp_repair.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # (c) 2015 Esther Martín - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class MrpRepair(models.Model):
