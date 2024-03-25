47dd3263fa8ca38c80ad0ef1361541b0de72fed5
Unai Beristain <unaiberistain@avanzosc.es>
2024-03-25 15:10:44 +0100

    [MIG] repair_warranty: Migration to 16.0

---
+++ repair_warranty/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018 Daniel Campos - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Repair Warranty",
+    "version": "16.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/mrp-repair-addons",
+    "depends": [
+        "repair",
+    ],
+    "data": [
+        "views/mrp_repair_view.xml",
+    ],
+    "installable": True,
+}
