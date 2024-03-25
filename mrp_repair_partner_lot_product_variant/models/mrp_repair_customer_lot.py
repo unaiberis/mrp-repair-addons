ee03fba6e9c22700edd73b7ba56d738f6e95e30b
Unai Beristain <unaiberistain@avanzosc.es>
2024-03-25 15:09:43 +0100

    [MIG] mrp_repair_partner_lot_product_variant: Migration to 16.0

--- mrp_repair_partner_lot_product_variant/models/mrp_repair_customer_lot.py
+++ mrp_repair_partner_lot_product_variant/models/mrp_repair_customer_lot.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import models, fields
+from odoo import models, fields
 
 
 class MrpRepairCustomerLot(models.Model):
