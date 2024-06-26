c62facb13d3fd827dad3b1a372354c12bee9d63f
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-26 12:20:42 +0200

    [IMP] mrp_repair_date: pre-commit stuff

--- mrp_repair_date/__manifest__.py
+++ mrp_repair_date/__manifest__.py
@@ -1,22 +1,22 @@
 # (c) 2015 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
-    'name': 'MRP Repair Date',
+    "name": "MRP Repair Date",
     "version": "16.0.1.0.0",
-    "license": 'AGPL-3',
-    "author": 'OdooMRP team,'
-              'AvanzOSC,'
-              'Serv. Tecnol. Avanzados - Pedro M. Baeza',
-    'website': "http://www.odoomrp.com",
+    "license": "AGPL-3",
+    "author": "OdooMRP team," "AvanzOSC," "Serv. Tecnol. Avanzados - Pedro M. Baeza",
+    "website": "https://github.com/avanzosc/mrp-repair-addons",
     "contributors": [
         "Ana Juaristi <anajuaristi@avanzosc.es>",
         "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
         "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
-        ],
-    'category': 'Manufacturing',
-    'depends': ['mrp_repair',
-                ],
-    'data': ['views/mrp_repair_view.xml',
-             ],
-    'installable': True,
+    ],
+    "category": "Manufacturing",
+    "depends": [
+        "mrp_repair",
+    ],
+    "data": [
+        "views/mrp_repair_view.xml",
+    ],
+    "installable": True,
 }
